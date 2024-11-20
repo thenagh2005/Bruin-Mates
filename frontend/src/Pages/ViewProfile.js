8910d01044fe51403f7d443ada8a99f0bb2949d8
thenagh2005 <anaghpandya@gmail.com>
2024-11-20 15:27:41 -0800

    Now, the curr user can't be searched in findpage. Also, entire profile is visible on profile page now.

--- frontend/src/Pages/ViewProfile.js
+++ frontend/src/Pages/ViewProfile.js
@@ -4,11 +4,24 @@ import '../Styles/Profile.css'
 import axios from "axios";
 
 const ViewProfile = () => {
+    const sleepyTimes = ['6 PM - 8 PM',
+                                '8 PM - 10 PM',
+                                '10 PM - 12 AM',
+                                '12 AM - 2 AM',
+                                "I'm Nocturnal HAHAHAHAHAHAHA" ];
+
     const [profile, setProfile] = useState("");
 
     const [currUser, setCurrUser] = useState([]);
+    const [age, setAge] = useState("");
     const [cleanliness, setCleanliness] = useState("");
     const [sleepTime, setSleepTime] = useState("");
+    const [smoking, setSmoking] = useState(false);
+    const [alcohol, setAlcohol] = useState(false);
+    const [roomType, setRoomType] = useState("");
+    const [building, setBuilding] = useState("");
+    const [occupancy, setOccupancy] = useState("");
+    
     /*
     useEffect(() => {
         fetch('http://localhost:4000/api/v1/user/view-profile', {
@@ -39,6 +52,15 @@ const ViewProfile = () => {
 
                 setCurrUser(response.data.user);
                 setCleanliness(response.data.user.preferences.cleanliness);
+                setSleepTime(sleepyTimes[parseInt(response.data.user.preferences.sleepTime, 10)-1]);
+                setAge(response.data.user.preferences.age);
+                setSmoking(response.data.user.preferences.smoking);
+                setAlcohol(response.data.user.preferences.alcohol);
+                setRoomType(response.data.user.preferences.roomType);
+                setBuilding(response.data.user.preferences.building);
+                setOccupancy(response.data.user.preferences.occupancy);
+
+                localStorage.setItem("username", response.data.user.name);
             } catch (error) {
                 console.error("Error fetching users:", error);
             }
@@ -54,6 +76,8 @@ const ViewProfile = () => {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/9-94702_user-outline-icon-clipart-png-download-profile-icon.png" style={{ width: '5vw', minWidth: '100px' }} alt="pfp" />
                     <h1>Name</h1>
                     <h2>{currUser.name}</h2>
+                    <h2>Age:</h2>
+                    <p>{age}</p>
                     <p>Insert bio here.</p>
                 </div>
                 <div>
@@ -63,7 +87,22 @@ const ViewProfile = () => {
                     <p>{cleanliness}</p>
 
                     <h2>Preferred Sleeping Time:</h2>
-                    <p>wat</p>
+                    <p>{sleepTime}</p>
+
+                    <h2>Smoking:</h2>
+                    <p>{smoking ? "Yes" : "No"}</p>
+
+                    <h2>Alcohol:</h2>
+                    <p>{alcohol ? "Yes" : "No"}</p>
+
+                    <h2>Room Type:</h2>
+                    <p>{roomType}</p>
+
+                    <h2>Building:</h2>
+                    <p>{building}</p>
+
+                    <h2>Occupancy:</h2>
+                    <p>{occupancy}</p>
                     
                     <a href="/profile-form">Edit your profile</a>
                 </div>
