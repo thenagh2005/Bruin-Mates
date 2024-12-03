d690b54bce04799b55b28431a22854eb9c2bcca0
Saatvik <saatvik.s12@gmail.com>
2024-12-02 19:14:05 -0800

    Change existing routes to reflect new match request schema. Route to create a request is still being worked on.

--- backend/controllers/matching-controllers.js
+++ backend/controllers/matching-controllers.js
@@ -1,33 +1,98 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.js');
+const MatchRequest = require('../models/MatchRequest.js')
 
 // WILL WORK ON THIS
-// async function processMatchRequest(req, res, next){
-//     console.log('HIT PROCESS MATCH REQUEST')
-//     try {
-//         const {sendingMatchToUserId} = req.body;
-//         const currUserId = res.locals.jwtData.id;
-//         const currUser = await User.findById(userId);
-//         const sendingMatchToUser = await User.findById(sendingMatchToUserId);
-//     } catch(error){
-//         console.log(error);
-//         return res.status(500).json({ message: 'ERROR', cause: error.message });
-//     }
-// }
-
-async function getAllUserMatches(res, res, next){
-    console.log('HIT')
+async function processMatchRequest(req, res, next){
+    console.log('HIT PROCESS MATCH REQUEST')
     try {
-        const userId = res.locals.jwtData.id;
-        const user = await User.findById(userId);
-        pending_matches = user.matches.filter(match => match.pending);
+        const { sendingMatchToUserId } = req.body;
+        const currUserId = res.locals.jwtData.id;
+        const currUser = await User.findById(userId);
+        const sendingMatchToUser = await User.findById(sendingMatchToUserId);
 
-        return res.status(200).json({ message: 'OK', pending_matches});
+        async.waterfall(
+            [
+                function (done) {
+                    crypto.randomBytes(20, (err, buf) => {
+                        var token = buf.toString("hex");
+                        done(err, token);
+                    });
+                },
+                function (token, done) {
+                    // add code here to add matches to the users data
+                },
+                function (token, user, done) {
+                    let smtpTransport = nodemailer.createTransport({
+                        service: "Gmail",
+                        auth: {
+                            user: process.env.GMAILACC,
+                            pass: process.env.GMAILAPPPW,
+                        },
+                    });
+                    let mailOptions = {
+                        to: sendingMatchToUser.email,
+                        from: '"Bruin Mates" bruinmates1919@gmail.com',
+                        subject: "Someone wants to match with you!",
+                        text:
+                            " SOMEONE WANTS TO CONNECT WITH YOU\n" +
+
+                            // insert link here
+                            "You can also view this message request  \n\n" +
+                            "Regards, \n" +
+                            "Bruin Mates",
+                    };
+                    smtpTransport.sendMail(mailOptions, (err) => {
+                        console.log("mail sent");
+                        console.log(
+                            "An e-mail has been sent to " +
+                            sendingMatchToUser.email +
+                            " with further instructions."
+                        );
+                        done(err, "done");
+                    });
+                },
+            ],
+            function (err) {
+                // error handling
+            }
+        );
+
     } catch(error){
         console.log(error);
         return res.status(500).json({ message: 'ERROR', cause: error.message });
     }
 }
 
-module.exports = {getAllUserMatches}
+async function getAllPendingMatches(res, res, next){
+    console.log('HIT')
+    try {
+        const userId = res.locals.jwtData.id;
+        const receivedRequests = await Match.find({ recipient_id: userId, status: 'pending' });
+
+        return res.status(200).json({ message: 'OK', receivedRequests});
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({ message: 'ERROR', cause: error.message });
+    }
+}
+
+async function getAllAcceptedMatches(res, res, next){
+    console.log('HIT')
+    try {
+        const userId = res.locals.jwtData.id;
+        const user = await User.findById(userId);
+        const acceptedMatches = await Match.find({ // both parties have agreed
+            $or: [{ requester_id: userId }, { recipient_id: userId }],
+            status: 'accepted'
+        });
+
+        return res.status(200).json({ message: 'OK', accepted_matches});
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({ message: 'ERROR', cause: error.message });
+    }
+}
+
+module.exports = {getAllAcceptedMatches, getAllPendingMatches, processMatchRequest};
--- backend/routes/matching-routes.js
+++ backend/routes/matching-routes.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const { verifyToken } = require("../utils/token-manager");
-const { getAllUserMatches } = require("../controllers/matching-controllers");
+const { getAllAcceptedMatches, getAllPendingMatches } = require("../controllers/matching-controllers");
 const router = express.Router();
-// const { processMatchRequest } = require("../controllers/matching-controllers.js");
 
-router.get('/', (req, res) => {
-    console.log('HIT');
-})
-// send email about matching request
-// router.post('/request-match/:id');
-// accept match route
-// show all matches for a particular user route
-router.get('/get-all-matches', verifyToken, getAllUserMatches);
+router.get('/get-accepted-matches', verifyToken, getAllAcceptedMatches);
+router.get('/get-pending-matches', verifyToken, getAllPendingMatches);
+
 
 module.exports = router;
